42d11d23ea1460b2cf3e6468b9be90fb2f4a6ab3
Ruslan Kryskiv <kryskiv23@gmail.com>
2023-06-25 09:46:33 +0300

    Working version of the application

---
+++ src/app/models/OrderAModel.java
@@ -0,0 +1,12 @@
+package app.models;
+
+import app.base.BaseModel;
+
+public class OrderAModel implements BaseModel {
+
+    @Override
+    public double getOrderCost(int quota,  double price) {
+
+        return quota * price;
+    }
+}
